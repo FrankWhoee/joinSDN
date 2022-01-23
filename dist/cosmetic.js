5d4e954712eb0ca7e6726fdbba305bec205ec1a0
Frank Hui <huifrank@live.com>
2022-01-23 01:15:14 -0800

    Finish website

--- dist/cosmetic.js
+++ dist/cosmetic.js
@@ -16,17 +16,81 @@ for (var i = 1; i <= 8; i++) {
   document.getElementById("cim" + i).src = "images/covers/" + r + ".jpg";
 }
 
+function changeRandomPicture() {
+  var i = Math.floor(Math.random() * 8) + 1;
+  var el = document.getElementById("cim" + i);
+  var curr = parseInt(el.src.split("/")[5].split(".")[0]);
+  selected.splice(selected.indexOf(curr), 1);
+  var r = Math.floor(Math.random() * 20);
+
+  while (selected.includes(r)) {
+    r = Math.floor(Math.random() * 20);
+  }
+
+  selected.push(r);
+  var tl = anime.timeline({
+    easing: 'easeOutExpo',
+    duration: 750
+  });
+  tl.add({
+    targets: [el],
+    opacity: 0,
+    complete: function complete(anim) {
+      el.src = "images/covers/" + r + ".jpg";
+    }
+  }).add({
+    targets: [el],
+    opacity: 1
+  });
+} // setInterval(changeRandomPicture, Math.random() * 1000);
+// setInterval(changeRandomPicture, Math.random() * 1000);
+
+
 function countStart() {
   fetch("/count").then(function (response) {
     return response.json();
   }).then(function (data) {
-    var demo = new _countUp.CountUp('playercount', data.count);
-    demo.start();
+    var pcount = new _countUp.CountUp('playercount', data.count);
+    pcount.start();
+  });
+}
+
+function sleep(time) {
+  return new Promise(function (resolve) {
+    return setTimeout(resolve, time);
+  });
+}
+
+function countMF() {
+  sleep(1000).then(function () {
+    document.getElementById("mfnum").innerHTML = 2;
+    sleep(1000).then(function () {
+      document.getElementById("mfnum").innerHTML = 1;
+      sleep(1000).then(function () {
+        var tl = anime.timeline({});
+        tl.add({
+          targets: '.mf',
+          opacity: 0,
+          complete: function complete() {
+            document.getElementById("joinsdnbutton").style.display = "inline";
+          }
+        }).add({
+          targets: '.mfbutton',
+          opacity: 1
+        });
+      });
+    });
   });
 }
 
 ScrollReveal().reveal('#playercount', {
   beforeReveal: countStart
+});
+ScrollReveal().reveal('.rankicons', {
+  distance: '100px'
+});
+ScrollReveal().reveal('#mfnum', {
+  beforeReveal: countMF
 });
 
 function updatePlayerCount() {
--- public/javascripts/cosmetic.js
+++ public/javascripts/cosmetic.js
@@ -11,16 +11,84 @@ for (var i = 1; i <= 8; i++) {
     document.getElementById("cim" + i).src = "images/covers/" + r + ".jpg";
 }
 
+function changeRandomPicture(){
+    var i = Math.floor(Math.random() * 8) + 1
+    var el = document.getElementById("cim" + i)
+    const curr = parseInt(el.src.split("/")[5].split(".")[0])
+    selected.splice(selected.indexOf(curr), 1);
+    var r = Math.floor(Math.random() * 20)
+    while (selected.includes(r)) {
+        r = Math.floor(Math.random() * 20)
+    }
+    selected.push(r)
+    var tl = anime.timeline({
+        easing: 'easeOutExpo',
+        duration: 750
+    });
+    tl.add({
+        targets: [el],
+        opacity: 0,
+        complete: function(anim) {
+            el.src = "images/covers/" + r + ".jpg"
+        }
+    }).add({
+        targets: [el],
+        opacity: 1
+    })
+
+
+}
+
+
+// setInterval(changeRandomPicture, Math.random() * 1000);
+// setInterval(changeRandomPicture, Math.random() * 1000);
+
 function countStart(){
     fetch("/count").then(response =>response.json()).then(data => {
-        var demo = new CountUp('playercount', data.count)
-        demo.start();
+        var pcount = new CountUp('playercount', data.count)
+        pcount.start();
+    })
+}
+
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function countMF(){
+    sleep(1000).then(()=>{
+        document.getElementById("mfnum").innerHTML = 2;
+        sleep(1000).then(()=>{
+            document.getElementById("mfnum").innerHTML = 1;
+            sleep(1000).then(()=>{
+                var tl = anime.timeline({})
+
+                tl.add({
+                    targets: '.mf',
+                    opacity: 0,
+                    complete: function () {
+                        document.getElementById("joinsdnbutton").style.display = "inline"
+                    }
+                }).add({
+                    targets: '.mfbutton',
+                    opacity: 1
+                })
+            })
+        })
     })
 }
 
 ScrollReveal().reveal('#playercount',{
     beforeReveal: countStart
 });
+
+ScrollReveal().reveal('.rankicons',{
+    distance: '100px'
+});
+
+ScrollReveal().reveal('#mfnum',{
+    beforeReveal: countMF
+});
+
 
 function updatePlayerCount(){
     fetch("/count").then(response =>response.json()).then(data => {
